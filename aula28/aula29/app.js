bb9a42f9d7830c6ae4bf6d1b53cf9b9f59a083a8
Andre Mota <144697456+andresgmota@users.noreply.github.com>
2023-09-21 11:57:54 -0300

    atualizei

--- aula28/aula29/app.js
+++ aula28/aula29/app.js
@@ -10,6 +10,12 @@ const nota1 = 9.0
 const nota2 = 10.0
 const media = (nota1*peso1 + nota2*peso2)/ (peso1+peso2)
 console.log(media.toString())
+console.log(media.toFixed(2))
 
 let num = Number(2)
-console.log(Number.isInteger(num))
+console.log(Number.isInteger(num))// verifica true ou flase
+
+//Função Math
+let lado = 4
+const areaQuadrado = Math.pow(lado, 2)
+console.log(areaQuadrado)
