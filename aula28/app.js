94f3b7ec86725c7ea72f5e26ff25e8dabfbde241
Andre Mota <144697456+andresgmota@users.noreply.github.com>
2023-09-21 11:08:02 -0300

    atualizações

--- aula28/app.js
+++ aula28/app.js
@@ -1,5 +1,5 @@
-let PI = 3.14
-let raio = 10
+const PI = 3.14
+const raio = 10
 let areaCirc = PI*raio*raio
 console.log(areaCirc)
-console.log("O valor da área da circunferência é " + areaCirc)
+console.log("O valor da área da circunferência é " + areaCirc + " m2.")
---
+++ aula28/aula29/app.js
@@ -0,0 +1,15 @@
+let a = 7
+let b = 94
+let temp = a
+a = b
+b = temp
+console.log(a, b)
+const peso1 = 1
+const peso2 = 2
+const nota1 = 9.0
+const nota2 = 10.0
+const media = (nota1*peso1 + nota2*peso2)/ (peso1+peso2)
+console.log(media.toString())
+
+let num = Number(2)
+console.log(Number.isInteger(num))
