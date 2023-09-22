0ab4a91ff72e33b6160e8f713fc21f5fe1b55492
Andre Mota <144697456+andresgmota@users.noreply.github.com>
2023-09-22 12:15:03 -0300

    atualização aula 50

---
+++ aula49-string/app.js
@@ -0,0 +1,10 @@
+const escola = "cod3r"
+console.log(escola.charAt(4))
+console.log(escola.charCodeAt(3))
+console.log(escola.lastIndexOf('3'))
+console.log(escola.substring(1))// a partir do 1
+console.log(escola.substring(0,3))//inclui o 0 exclui a partir do 3
+console.log("Escola ".concat(escola).concat("!"))
+console.log('Escola ' + escola + '!')
+console.log(escola.replace(3, 'e'))
+console.log('Andre, Civania, Pedro'.split(','))
---
+++ aula50-templatestring/app.js
@@ -0,0 +1,8 @@
+const nome = 'Andre'
+const concatenacao = 'Olá ' + nome + '!'
+const template = `Olá ${nome}!`
+console.log(template)
+console.log(`1 + 1 = ${1 + 1}`)
+const texto = 'Colocando em maiúsculas' 
+const textoEmMaiusculas = texto.toUpperCase()
+console.log(textoEmMaiusculas)
